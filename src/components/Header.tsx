73396d3635c689587fe4fe099802af2cd8dc1d5c
skmajumder <skmajumder.cse@gmail.com>
2025-10-09 18:34:45 +0600

    <Header/> component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import * as React from "react";
+import { HomeIcon, File, UsersRound } from "lucide-react";
+import Link from "next/link";
+import NavButton from "./NavButton";
 
-const Header = () => {
-  return <div>Header</div>;
-};
+export type HeaderProps = React.ComponentPropsWithoutRef<"header"> & {};
 
-export default Header;
+export default function Header({ className, ...props }: HeaderProps) {
+  return (
+    <header
+      className={`animate-slide sticky top-0 z-20 h-12 border-b bg-background p-2 ${className ?? ""}`}
+      {...props}
+    >
+      <div className="flex h-8 w-full items-center justify-between">
+        <div className="flex items-center gap-2">
+          <NavButton href="/home" label="Home" icon={HomeIcon} />
+          <Link
+            href="/home"
+            className="ml-0 flex items-center justify-center gap-2"
+            title="Home"
+          >
+            <h2 className="m-0 mt-1 hidden text-xl font-bold sm:block">
+              Computer Repair Shop
+            </h2>
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <NavButton href="/tickets" label="Tickets" icon={File} />
+          <NavButton href="/customers" label="Customers" icon={UsersRound} />
+        </div>
+      </div>
+    </header>
+  );
+}
